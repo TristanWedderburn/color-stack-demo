3ae5a585f22a360cc5844c300c826db7a331341a
Tristan Wedderburn <tristan.wedderburn@mongodb.com>
2021-09-30 15:16:02 -0400

    comment out content-type middleware

--- routers/book.js
+++ routers/book.js
@@ -5,6 +5,9 @@ const router = express.Router() //Express Router is also considered a middleware
 const Book = require('../models/book')
 const Author = require('../models/author')
 
+// Add mongoose for ObjectId
+const mongoose = require('mongoose');
+
 // Example of middleware
 // Other examples could be implementing Authentication and checking if the cookies in our request are valid on the server. We can use third-party libraries to accomplish this!
 
@@ -14,24 +17,29 @@ router.use((req, res, next) => {
     // For example, we can intercept and validate the content-type of the request being made for requests to create or update books
     console.log("In the book route")
     console.log(`request type of ${req.method}`)
-
-    if (['POST', 'PUT'].includes(req.method)) {
-        console.log("Potentially dangerous request verb")
-        if (req.headers['content-type'] !== 'application/json') {
-            res.status(400).send('Sorry, the server requires the Content-Type to be application/json ')
-        }
-        console.log("But validation passed")
-        next()
-    } 
     next()
 })
 
-const generateIsbn = () => {
-    return (req, res, next) => {
-        console.log("Generating ISBN for book")
-        req.isbn = "12345"
-        next()
-    }
+// const checkContentType = (res, req, next) => {
+//     console.log(res)
+//     console.log(req)
+//     console.log("Content-Type", req.headers['content-type'])
+
+//     if (['POST', 'PUT'].includes(req.method)) {
+//         console.log("Potentially dangerous request verb")
+//         if (req.headers['content-type'] !== 'application/json') {
+//             res.status(400).send('Sorry, the server requires the Content-Type to be application/json ')
+//             return
+//         }
+//         console.log("But, validation passed")
+//         next()
+//     } 
+// }
+
+const generateIsbn = (req, res, next) => {
+    console.log("Generating ISBN for book")
+    req.isbn = "12345"
+    next()
 }
 
 // Let's talk about callbacks here
@@ -40,9 +48,9 @@ const generateIsbn = () => {
 // However, some funtions aren't immediate computations or can potentially take a while to return, such as interacting with our database.
 // For this reason, we want to use the async/await feature (a new way to implement asynchronous code in js that makes the code cleaner)
 // easy error handling using a try and catch block
-router.post('/', () => generateIsbn(), async(req, res) => {
+router.post('/', generateIsbn, async (req, res) => {
     // Example post request to create a book for an author:
-    // curl -X POST -H "Content-Type: application/json" -d '{"author": "1234", "title": "ColorStack Dictionary", "summary": "This is our books summary", "isbn": "978-3-16-148410-0" }' http://localhost:4200/book/
+    // curl -X POST -H "Content-Type: application/json" -d '{"author": "6153d82453d20de21c62bb68", "title": "ColorStack Dictionary 3", "summary": "This is our books summary", "isbn": "978-3-16-148410-0" }' http://localhost:4200/book/
 
     try {
         // validate req.body using the model
@@ -51,18 +59,20 @@ router.post('/', () => generateIsbn(), async(req, res) => {
         await newBook.save()
 
         // Push the book to the author that owns it
-        const author = await Author.findById({_id: newBook.author})
+        const authorId = mongoose.Types.ObjectId(newBook.author)
+        const author = await Author.findById(authorId)
         author.books.push(newBook)
         await author.save()
 
         res.status(200).json(newBook)
     } catch (err) {
-        res.status(400).json({error: err})
+        console.log("err", err)
+        // res.status(400).json({error: err})
     }
 })
 
 router.get('/', async(req, res) => {
-    // request to localhost:4200/book?title=ColorStack+Dictionary
+    // Example request: http://localhost:4200/book?title=ColorStack%20Dictionary
 
     try {
         const {title} = req.query
